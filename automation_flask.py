b9b91524c4db12c0d1fa3981391bc6f19b531a9c
Kelvin Piña <kelvinjps10@gmail.com>
2021-12-08 09:50:09 -0500

    method create files and create virtualenv

--- automation_flask.py
+++ automation_flask.py
@@ -3,12 +3,29 @@ import os
 
 editor="code"
 
+def create_files():
+    os.system("touch Procfile")
+    os.system("touch requirements.txt")
+    os.system("touch README.md")
+
+def create_virtaulenv():
+    os.system("virtualenv venv")
+    os.system("source venv/bin/activate")
+
+
+
 name_project=input("Enter the name of the project: ")
-auto_project.create_folder(name_project)
-auto_project.create_folder("src")
-os.system("touch Procfile")
-os.system("touch requirements.txt")
-os.system("touch README.md")
+#if folder exists
+
+if(auto_project.create_folder(name_project)==True):
+ auto_project.create_folder("src")
+ create_files()
+ auto_project.create_git_repo()
+ auto_project.create_github_repo()
+ auto_project.open_editor(editor)
+
+else:
+     print("Folder already exist")
 
 
 
