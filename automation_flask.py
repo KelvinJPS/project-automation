a4f8d839f24e780c8ba099c60f0b254738f4a939
Kelvin Piña <kelvinjps10@gmail.com>
2021-12-12 22:59:09 -0500

    automatation for flask projects: create folders, install dependencies and fill necesary files

--- automation_flask.py
+++ automation_flask.py
@@ -4,7 +4,7 @@ from automation_project import auto_project
 class flask_project():
 
 
- def create_files():
+ def create_flask_files():
     os.system("touch Procfile")
     os.system("touch requirements.txt")
     os.system("touch README.md")
@@ -21,8 +21,23 @@ class flask_project():
     os.system("{0} install gunicorn".format(pip))
     os.system("{0} install freezer".format(pip))
     os.system("{0} freeze > requirements.txt".format(pip))
- 
+
   
  def fill_runtime():
     python3 = "venv/bin/python3"
     os.system("{0} --version > runtime.txt".format(python3))
+
+ def fill_Procfile():
+    os.system("echo 'web: gunicorn app:app' > Procfile")
+
+ def fill_python_flask_base():
+     #import dependencies
+     os.system("echo from flask import Flask, render_template, request, redirect, url_for, flash > app.py")
+
+
+ def create_flask_folders():
+    os.system("mkdir -p static/css static/js static/img templates")
+    os.system("touch static/css/style.css")
+    os.system("touch static/js/main.js")
+    os.system("touch templates/index.html")
+   
--- create_flask_project.py
+++ create_flask_project.py
@@ -3,12 +3,26 @@ from automation_flask import flask_project
 import os
 
 name_project=input("Enter the name of the project: ")
-#if folder exists
+#if folder not exists
 if(auto_project.create_folder(name_project)!=False):
+  #create flask folders
   auto_project.create_folder("src")
-  flask_project.create_files()
+  flask_project.create_flask_files()
+  flask_project.create_flask_folders()
+  #create virtual environment and install dependencies
   flask_project.create_virtual_environment()
+  flask_project.install_dependencies()
+  #fill flask files
+  flask_project.fill_Procfile()
   flask_project.fill_runtime()
+  flask_project.fill_python_flask_base()
+
+  #git flask project
+  auto_project.create_git_repo()
+  auto_project.create_github_repo()
+
+  #opèn project
+  auto_project.open_editor(auto_project.editor)
 
 else:
      print("Folder already exist")
